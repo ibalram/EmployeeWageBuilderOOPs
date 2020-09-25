a72105fb0448b1a3999b4de34aae27faeaf1924a
ibalram <balramsinghrathore3@gmail.com>
2020-09-26 01:51:40 +0530

    Refactored: Change code structure to write class method

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -3,16 +3,13 @@ package com.cg;
 import java.util.Random;
 
 public class EmpWageBuilder {
+	static final int IS_FULL_TIME = 1;
+	static final int IS_PART_TIME = 2;
+	static int EMP_RATE_PER_HOUR = 20;
+	static int NUM_OF_WORKING_DAYS = 20;
+	static int MAX_HRS = 10;
 
-	public static void main(String[] args) {
-		System.out.println("Employee Wage Builder");
-		System.out.println("----------------------");
-		final int IS_FULL_TIME = 1;
-		final int IS_PART_TIME = 2;
-		int EMP_RATE_PER_HOUR = 20;
-		int NUM_OF_WORKING_DAYS = 20;
-		int MAX_HRS = 10;
-
+	public static int computeEmpWage() {
 		Random rand = new Random();
 		int totalEmpHrs = 0;
 		int dayCount = 0, empHrs = 0;
@@ -28,9 +25,16 @@ public class EmpWageBuilder {
 				break;
 			}
 			totalEmpHrs += empHrs;
-			System.out.println("Day#: " + dayCount + " Emp Hr: "+empHrs);
+			System.out.println("Day#: " + dayCount + " Emp Hr: " + empHrs);
 		}
 		int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Monthly Employee Wage: " + totalEmpWage);
+		return totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Employee Wage Builder");
+		System.out.println("----------------------");
+		computeEmpWage();
 	}
 }
