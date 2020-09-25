ab5bd5b6fd5001bc44a15b1b62a9a67c061e429c
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 23:21:00 +0530

    Refactored: Change if-else to switch-case statement

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -7,17 +7,21 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Employee Wage Builder");
 		System.out.println("----------------------");
-		int IS_FULL_TIME = 1;
-		int IS_PART_TIME = 2;
+		final int IS_FULL_TIME = 1;
+		final int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 
 		Random rand = new Random();
 		int empCheck = rand.nextInt(3);
 		int empHrs = 0;
-		if (empCheck == IS_FULL_TIME)
+		switch (empCheck) {
+		case IS_FULL_TIME:
 			empHrs = 8;
-		else if (empCheck == IS_PART_TIME)
+			break;
+		case IS_PART_TIME:
 			empHrs = 4;
+			break;
+		}
 		int dailyWage = empHrs * EMP_RATE_PER_HOUR;
 		System.out.println("Employee Wage: " + dailyWage);
 	}
