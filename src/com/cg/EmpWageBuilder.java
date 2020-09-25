4bb4b84ba44fd0f3dec4817529c8aced6643a04d
ibalram <balramsinghrathore3@gmail.com>
2020-09-25 22:57:08 +0530

    Added: Check employee is present or not

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -1,9 +1,18 @@
 package com.cg;
 
-public class EmpWageBuilder {
+import java.util.Random;
 
+public class EmpWageBuilder {
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome!\nEmployee Wage Builder Program");
+		int IS_FULL_TIME = 1;
+		Random rand = new Random();
+		int empCheck = rand.nextInt(2);
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 
 }
